e440c50d4587ce6932bb4f68b3c517c8d9637a43
EvATive7 <evative7@gmail.com>
2023-05-06 12:43:09 +0800

    开发 未完成工作

--- MessageGetterLibrary/StorageHelper/MessageInfo.cs
+++ MessageGetterLibrary/StorageHelper/MessageInfo.cs
@@ -11,6 +11,19 @@ namespace MessageGetter
         public MessageInfo() { }
         
         public CreatedByType MessageCreatedBy { get; set; }
+        public bool Hide { get; set; } = false;
 
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if (this == obj) return true;
+            if (obj.GetType() != typeof(MessageInfo)) return false;
+
+            var msgifo = obj as MessageInfo;
+            if (msgifo.MessageCreatedBy != this.MessageCreatedBy) return false;
+            if (msgifo.Hide != this.Hide) return false;
+
+            return true;
+        }
     }
 }
