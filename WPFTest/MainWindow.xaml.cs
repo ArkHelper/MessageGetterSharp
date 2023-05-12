46abada83d356c7b6fbbf2fed1233a0714f80f9e
EvATive7 <evative7@gmail.com>
2023-05-12 23:16:19 +0800

    未完成工作 构建Demo

--- WPFTest/MainWindow.xaml.cs
+++ WPFTest/MainWindow.xaml.cs
@@ -1,20 +1,11 @@
 ﻿using MessageGetter;
 using MessageGetter.Medias;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFTest
 {
@@ -27,87 +18,40 @@ namespace WPFTest
         {
             InitializeComponent();
 
+            /* 设置配置 */
             Getter.Configuration = new Configuration()
             {
-                AutoDownloadVideo = false,
-                AutoDownloadPicture = true,
-                Interval = 60000,
-                RootDir = Directory.GetCurrentDirectory() + @"\message"
-                
+                AutoDownloadVideo = false, //设置不自动下载视频
+                AutoDownloadPicture = true, //设置自动下载图片
+                Interval = 60000, //设置刷新器定时（单位：毫秒）
+                RootDir = Directory.GetCurrentDirectory() + @"\message", //设置文件保存根目录
+
+                /* 设置筛选条件（不符合筛选条件的也将进入队列，但是没有新消息提醒
+                 * 此处筛选条件是通过传入一个[具有一个形参一个输出的函数]实现的
+                 * 当符合筛选条件（即保留该消息）要返回True。*/
+                Filter = new System.Func<Message, bool>((message) =>
+                {
+                    bool @in = true;
+
+                    if (message.Text.Contains("微博抽奖平台")) @in = false;
+                    return true;
+                })
+
             };
 
-            Getter.AddUser(new Weibo("6279793937"));
-            Getter.NewMessageAdded += Getter_NewMessageAdded;
-            Getter.StartInterval();
+            Getter.AddUser(new Weibo("6279793937")); //添加用户
+            Getter.NewMessageAdded += Getter_NewMessageAdded; //绑定事件
+            Getter.StartInterval();//启动刷新
         }
 
         private void Getter_NewMessageAdded(Message message, MessageInfo messageInfo)
         {
-            Application.Current.Dispatcher.Invoke(() =>
+            Application.Current.Dispatcher.Invoke(() => MessageBoxs.Children.Add(new MessageCardUI(message)));
+            Task.Run(() =>
             {
-                ListBox listBox = new ListBox();
-                message.Medias.ForEach(m =>
-                {
-                    TextBlock textBlock = new TextBlock();
-                    if (m.GetType() == typeof(Picture))
-                    {
-                        m = m as Picture;
-                        textBlock.Inlines.Add("图片：" + m.ID + ": ");
-
-                        Run run = new Run();
-                        run.Text = "0";
-
-                        if (m.Local != null)
-                        {
-                            run.Text = "文件已存在，不再下载";
-                        }
-                        else
-                        {
-                            m.DownloadProgressChanged += (s, e) =>
-                            {
-                                Application.Current.Dispatcher.Invoke(() =>
-                                {
-                                    run.Text = e.ProgressPercentage.ToString();
-                                });
-                            };
-                            m.DownloadCompleted += (s, e) =>
-                            {
-                                Application.Current.Dispatcher.Invoke(() =>
-                                {
-                                    run.Text = "下载结束";
-                                });
-                            };
-                            //m.Download();
-                        }
-                        textBlock.Inlines.Add(run);
-                    }
-                    if (m.GetType() == typeof(Video))
-                    {
-
-                        textBlock.Inlines.Add("视频：" + m.ID + "：不下载" + "，缩略图：" + m.View.ID);
-
-                    }
-
-                    listBox.Items.Add(new ListBoxItem() { Content = textBlock });
-                });
-
-                this.box.Children.Add(
-                    new WrapPanel()
-                    {
-                        Children =
-                        {
-
-                            new TextBlock()
-                            {
-                                Text = message.Text
-                            },
-                            listBox
-                        },
-
-                    }) ;
+                Thread.Sleep(1000);
+                message.Text = "666666";
             });
-
-
         }
     }
 }
---
+++ WPFTest/MessageCardUI.xaml.cs
@@ -0,0 +1,39 @@
+﻿using MessageGetter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTest
+{
+    /// <summary>
+    /// MessageCardUI.xaml 的交互逻辑
+    /// </summary>
+    public partial class MessageCardUI : UserControl
+    {
+        public static readonly DependencyProperty BindingPersonProperty =
+        DependencyProperty.Register("BindingPerson", typeof(Message), typeof(UserControl), new PropertyMetadata(null));
+
+        public Message BindingMessage
+        {
+            get { return (Message)GetValue(BindingPersonProperty); }
+            set { SetValue(BindingPersonProperty, value); }
+        }
+
+        public MessageCardUI(Message message)
+        {
+            InitializeComponent();
+            DataContext = message;
+        }
+    }
+}
