1cd979f435f6e64d47e785839e7b92609afed0b3
EvATive7 <evative7@gmail.com>
2023-05-13 12:30:27 +0800

    实现 Demo时间转换器

--- WPFTest/Converter/DateTimeToFormatStringConverter.cs
+++ WPFTest/Converter/DateTimeToFormatStringConverter.cs
@@ -2,24 +2,32 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace WPFDemo.Converter
 {
-    internal class DateTimeToFormatStringConverter
+    internal class DateTimeToFormatStringConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value == null || parameter == null)
-                return false;
-            return value.ToString() == parameter.ToString();
+            if (value == null)
+                throw new ArgumentNullException("value can not be null");
+
+            string @return = string.Empty;
+            var input = (DateTime)value;
+
+            if (input.Year != DateTime.Now.Year) @return += input.ToString("yyyy年");
+            @return += input.ToString("M") + " " + input.ToString("HH:mm");
+
+            return @return;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
-            return parameter;
         }
     }
 }
